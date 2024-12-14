9d9c080451db03fcea12106ccc25a8bd1ad2c08a
ede1998 <online@erik-hennig.me>
2024-12-14 22:54:40 +0100

    feat: Send signal out via IR LED

--- src/bin/async_main.rs
+++ src/bin/async_main.rs
@@ -15,6 +15,8 @@ use embassy_time::{Duration, Ticker};
 
 extern crate alloc;
 
+const _EXAMPLE_SIGNAL: &[u8] = b"000000000000000000111111111111111111001110011001110011001111111001100011001100111001100111001100111111100110011100110011100111111100111111100111111000111111000110011001110011111110011001110011001100011111100011111100111111100111111111111111";
+
 #[main]
 async fn main(spawner: Spawner) {
     let peripherals = esp_hal::init({
@@ -43,11 +45,10 @@ async fn main(spawner: Spawner) {
     // TODO: Spawn some tasks
     let _ = spawner;
     let mut rx = Input::new(peripherals.GPIO25, esp_hal::gpio::Pull::Up);
-    let _tx = Output::new(peripherals.GPIO17, esp_hal::gpio::Level::Low);
+    let mut tx = Output::new(peripherals.GPIO17, esp_hal::gpio::Level::Low);
 
     loop {
         // g25
-        // g17
         let mut signal: Vec<u8, 1_000> = heapless::Vec::new();
         // Min. pulse duration = 500us -> double it to protect against aliasing
         const TICK_RATE: Duration = Duration::from_micros(250);
@@ -69,6 +70,18 @@ async fn main(spawner: Spawner) {
             .unwrap();
 
         info!("{}", SignalPrinter(&signal[..last_sample]));
+
+        // g17
+        ticker.reset();
+        for &value in &signal[..last_sample] {
+            if value == 0 {
+                tx.set_low();
+            } else {
+                tx.set_high();
+            }
+            ticker.next().await;
+        }
+        tx.set_low();
     }
 
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/v0.22.0/examples/src/bin
