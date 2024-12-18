b27b428bc1b262416b2298ec2101c1a60e1bf7e9
ede1998 <online@erik-hennig.me>
2024-12-18 21:35:16 +0100

    feat: Add download button

--- website/script.js
+++ website/script.js
@@ -1,3 +1,8 @@
+function recordNewSignal() {
+    const name = prompt("Please enter signal name:");
+    alert(`Signal '${name}' created`);
+}
+
 function replaySignal(button, id) {
     const image = button.children[0];
     image.src = "sending.svg";
@@ -9,12 +14,39 @@ function replaySignal(button, id) {
 function renameSignal(id) {
     const name = document.querySelector(`.card:nth-child(${id})>h1`).textContent;
     const new_name = prompt("Please enter new signal name:", name);
-    if (new_name ?? "" != "") {
+    if (new_name ?? "" !== "") {
         document.querySelector(`.card:nth-child(${id})>h1`).textContent = new_name;
     }
 }
 
+function downloadSignal(id) {
+    const name = document.querySelector(`.card:nth-child(${id})>h1`).textContent;
+    downloadFile(`${name}.txt`, "010101010101110");
+}
+
 function deleteSignal(id) {
     const name = document.querySelector(`.card:nth-child(${id})>h1`).textContent;
-    alert(`Signal '${name}' deleted`);
+    if (confirm(`Do you really want to delete signal ${name}?`)) {
+        alert(`Signal '${name}' deleted`);
+    }
+}
+
+function downloadFile(name, contents, mime_type) {
+    mime_type = mime_type || "text/plain";
+
+    const blob = new Blob([contents], { type: mime_type });
+
+    const download_link = document.createElement('a');
+    download_link.download = name;
+    download_link.href = window.URL.createObjectURL(blob);
+    download_link.onclick = function (e) {
+        // revokeObjectURL needs a delay to work properly
+        const that = this;
+        setTimeout(() => {
+            window.URL.revokeObjectURL(that.href);
+        }, 1500);
+    };
+
+    download_link.click();
+    download_link.remove();
 }
