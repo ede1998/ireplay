2fb48442c58cba08576292d094b92163c42bbe51
ede1998 <online@erik-hennig.me>
2024-12-18 23:07:28 +0100

    feat: Make it dynamic

--- test_webserver.py
+++ test_webserver.py
@@ -1,7 +1,62 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, request
+import random
 
 app = Flask(__name__, static_folder="website")
 
-@app.route('/')
-def hello():
+def invert(value: int) -> int:
+    if value == 0:
+        return 1
+    return 0
+
+def generate_signal(name: str) -> dict[str, any]:
+    length = 1000
+    flip_count = random.randint(10, 100)
+    flip_positions = {random.randrange(length) for _ in range(flip_count)}
+    value = random.choice([0, 1])
+
+    return {
+        "name": name,
+        "curve": [(value := invert(value)) if i in flip_positions else value for i in range(length)],
+    }
+
+ALL_SIGNALS: dict[int, dict[str, any]] = {
+    id: generate_signal(name) for id, name in enumerate(["Frank", "Peter", "Harald", "Matthias", "Mark", "Lars"])
+}
+NEXT_ID = len(ALL_SIGNALS)
+
+def print_signal_ids():
+    app.logger.info(f"Available signal ids: {','.join([str(id) for id in ALL_SIGNALS])}")
+
+@app.get("/")
+def website():
     return redirect("website/index.html", code=302)
+
+@app.get("/signals")
+def signals():
+    return ALL_SIGNALS
+
+@app.delete("/signals/<int:id>")
+def delete_signal(id: int):
+    del ALL_SIGNALS[id]
+    return "", 200
+
+@app.post("/signals/<int:id>")
+def rename_signal(id: int):
+    new_name = request.get_data(as_text=True)
+    ALL_SIGNALS[id]["name"] = new_name
+    return "", 200
+
+@app.post("/signals")
+def record_new_signal():
+    global NEXT_ID
+    name = request.get_data(as_text=True)
+    signal = generate_signal(name)
+    ALL_SIGNALS[NEXT_ID] = signal
+    NEXT_ID += 1
+    print_signal_ids()
+    return { NEXT_ID - 1: signal}, 201
+
+@app.put("/signals/<int:id>")
+def replay_signal(id: int):
+    print(f"Replaying signal {ALL_SIGNALS[id]["name"]}")
+    return "", 200
--- website/script.js
+++ website/script.js
@@ -1,21 +1,87 @@
-function recordNewSignal() {
-    const name = prompt("Please enter signal name:");
-    alert(`Signal '${name}' created`);
+let ALL_SIGNALS = {}
+
+
+async function loadAllSignals() {
+    const request_url = `${window.location.origin}/signals`;
+    try {
+        const response = await fetch(request_url);
+        const signal_data = await response.json();
+        return signal_data;
+    } catch (error) {
+        console.log("Failed to retrieve signals");
+        return {};
+    }
 }
 
-function replaySignal(button, id) {
+function appendSignal(id, signal) {
+    const signal_card = `<article class="card" id="signal-${id}">
+        <h1>${signal.name}</h1>
+        <img src="recording.svg" alt="IR signal curve">
+        <div class="tools">
+            <button onclick="replaySignal(this, ${id});"><img src="play.svg" alt="Replay IR signal" /></button>
+            <button onclick="renameSignal(${id});"><img src="edit.svg" alt="Rename IR signal" /></button>
+            <button onclick="downloadSignal(${id});"><img src="save.svg" alt="Download IR signal" /></button>
+            <button onclick="deleteSignal(${id});"><img src="trash.svg" alt="Delete IR signal" /></button>
+        </div>
+    </article>`;
+
+    const gallery = document.querySelector(".cards");
+    const add_button = gallery.querySelector(".card:last-child")
+    add_button.insertAdjacentHTML("beforebegin", signal_card)
+}
+
+async function initialize() {
+    ALL_SIGNALS = await loadAllSignals();
+    for (const [id, signal] of Object.entries(ALL_SIGNALS)) {
+        appendSignal(id, signal);
+    }
+}
+
+async function recordNewSignal() {
+    const name = prompt("Please enter signal name:");
+    const request_url = `${window.location.origin}/signals`;
+    try {
+        await fetch(request_url, {
+            method: "POST",
+            headers: { 'Content-Type': 'text/plain' },
+            body: name,
+        });
+    } catch (error) {
+        console.log("Failed to create new signal");
+        return;
+    }
+    alert(`Created signal '${name}'`);
+    location.reload();
+}
+
+async function replaySignal(button, id) {
     const image = button.children[0];
     image.src = "sending.svg";
     setTimeout(() => {
         image.src = "play.svg";
     }, 100);
+
+    const request_url = `${window.location.origin}/signals/${id}`;
+    try {
+        await fetch(request_url, { method: "PUT" });
+    } catch (error) {
+        console.log("Failed to replay signal");
+    }
 }
 
-function renameSignal(id) {
-    const name = document.querySelector(`.card:nth-child(${id})>h1`).textContent;
+async function renameSignal(id) {
+    const name = ALL_SIGNALS[id].name;
     const new_name = prompt("Please enter new signal name:", name);
-    if (new_name ?? "" !== "") {
-        document.querySelector(`.card:nth-child(${id})>h1`).textContent = new_name;
+    if ((new_name ?? "") === "") {
+        return;
+    }
+
+    const request_url = `${window.location.origin}/signals/${id}`;
+    try {
+        await fetch(request_url, { method: "POST", body: new_name });
+        document.querySelector(`#signal-${id}>h1`).textContent = new_name;
+    } catch (error) {
+        console.log("Failed to rename signal");
     }
 }
 
@@ -24,10 +90,19 @@ function downloadSignal(id) {
     downloadFile(`${name}.txt`, "010101010101110");
 }
 
-function deleteSignal(id) {
-    const name = document.querySelector(`.card:nth-child(${id})>h1`).textContent;
-    if (confirm(`Do you really want to delete signal ${name}?`)) {
+async function deleteSignal(id) {
+    const name = ALL_SIGNALS[id].name;
+    if (!confirm(`Do you really want to delete signal ${name}?`)) {
+        return;
+    }
+
+    const request_url = `${window.location.origin}/signals/${id}`;
+    try {
+        await fetch(request_url, { method: "DELETE" });
         alert(`Signal '${name}' deleted`);
+        document.getElementById(`signal-${id}`).remove();
+    } catch (error) {
+        console.log("Failed to delete signal");
     }
 }
 
